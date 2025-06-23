70c889bed16d422781f83f81bcc408a69ae48844
Yash Kumar Rana <2200520100072@ietlucknow.ac.in>
2025-06-23 17:24:10 +0530

    redirectfileadd

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,7 +3,7 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './', 
+  base: '/',  // ✅ Important for Vercel
   plugins: [
     tailwindcss(),
     react()
